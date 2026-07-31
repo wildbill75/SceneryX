c1d69e9199e461d1d123b8895e87af200b5e5110
wildbill75 <bertrand.lecabec@gmail.com>
2026-07-31 15:34:29 +0200

    Fix missing urllib import in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import re
+import urllib.request
 import webview
 from scanner import run_scan, get_settings, save_settings, load_ratings, save_rating, save_custom_price, load_custom_prices, get_default_gsx_path, load_airport_database, SPECIAL_BUNDLE_MAP
 
